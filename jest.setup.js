3cfb6175e66376e0923c53d1ea379f16e30cfa21
Matt O'Keefe <matt@okeefe.dev>
2023-12-28 11:36:30 -0600

    cleaning extras

--- jest.setup.js
+++ jest.setup.js
@@ -4,24 +4,14 @@ const { wait } = require('./test/util');
 
 module.exports = async (globalConfig) => {
   const orgTimeout = globalConfig.testTimeout;
-  globalConfig.testTimeout = 500000;
-  const url = await mongoUnit.start({
+  globalConfig.testTimeout = 100000;
+  await mongoUnit.start({
     version: '6.0.12',
     port: Math.floor(Math.random() * 10000) + 10000,
   });
 
-  await wait(1000);
-  console.log('$$$$$$', url, ' ==== ', mongoUnit.getUrl());
-
-  const client = new MongoClient(mongoUnit.getUrl(), {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
   process.env.MONGO_URI = mongoUnit.getUrl();
   process.env.AWARDCO_API_KEY = 'test_123';
-
-  // Set reference to mongod in order to close the server during teardown.
-  globalThis.MONGO_CLIENT = client;
 
   globalConfig.testTimeout = orgTimeout;
 };
--- jest.teardown.js
+++ jest.teardown.js
@@ -2,7 +2,6 @@ const mongoUnit = require('mongo-unit');
 
 module.exports = async () => {
   console.log('Tearing down unit testing');
-  await globalThis.MONGO_CLIENT.close();
   await mongoUnit.stop();
   console.log('Torn down');
 };
