351d8629097c6d328652f40c247b77149043edfa
Matt O'Keefe <matt@okeefe.dev>
2024-01-03 10:04:18 -0600

    Adding helper to not load tests

--- index.js
+++ index.js
@@ -1,23 +1,55 @@
+/* eslint-disable no-console */
+/* eslint-disable no-continue */
 const fs = require('fs');
 const path = require('path');
+
+function loadScripts(robot, directory) {
+  const results = [];
+
+  fs.readdirSync(directory).forEach((file) => {
+    const filePath = path.join(directory, file);
+    const stats = fs.statSync(filePath);
+
+    if (stats.isDirectory()) {
+      // Recurse into subdirectory
+      results.push(...loadScripts(robot, filePath));
+    } else if (!file.endsWith('.test.js') && path.extname(file) === '.js') {
+      // Load .js files that don't export a class
+      // eslint-disable-next-line import/no-dynamic-require, global-require
+      const exported = require(filePath);
+      if (
+        exported instanceof Function &&
+        !exported.toString().startsWith('class')
+      ) {
+        results.push(robot.loadFile(directory, file));
+      }
+    }
+  });
+
+  return results;
+}
 
 module.exports = (robot, scripts) => {
   const scriptsPath = path.resolve(__dirname, 'src');
   return fs.exists(scriptsPath, (exists) => {
-    const results = [];
     if (exists) {
-      const ref = fs.readdirSync(scriptsPath);
-      for (let i = 0, len = ref.length; i < len; i++) {
-        const script = ref[i];
-        if (scripts && scripts.indexOf('*') < 0) {
-          if (scripts.indexOf(script) > -1) {
-            results.push(robot.loadFile(scriptsPath, script));
-          }
-        } else {
-          results.push(robot.loadFile(scriptsPath, script));
+      const scriptsToLoad =
+        scripts && scripts.indexOf('*') < 0 ? scripts : null;
+      const loadedScripts = loadScripts(robot, scriptsPath);
+
+      // Filter out scripts that weren't loaded
+      if (scriptsToLoad) {
+        const missingScripts = scriptsToLoad.filter(
+          (script) => !loadedScripts.includes(script),
+        );
+        if (missingScripts.length > 0) {
+          console.error(
+            `Hubot couldn't find the following scripts: ${missingScripts.join(
+              ', ',
+            )}`,
+          );
         }
       }
     }
-    return results;
   });
 };
